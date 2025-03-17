fa5c419ef2f04e2cd776f91efd26e24925d57338
Rishabhdaliya <14rishabhdaliya@gmail.com>
2025-03-17 11:29:04 +0530

    Update import paths for utility functions and components to relative paths

--- src/components/ui/autoSearch.jsx
+++ src/components/ui/autoSearch.jsx
@@ -3,8 +3,8 @@
 import * as React from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
 
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { cn } from "../../lib/utils";
+import { Button } from "../../components/ui/button";
 import {
   Command,
   CommandEmpty,
@@ -12,12 +12,12 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/components/ui/command";
+} from "../../components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover";
+} from "../../components/ui/popover";
 
 export function AutoSearch({ cities }) {
   const [open, setOpen] = React.useState(false);
